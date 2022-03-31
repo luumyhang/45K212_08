3c6f9a7550a721f40aec7f1146108675a3b19542
luumyhang <85682767+luumyhang@users.noreply.github.com>
2022-03-31 23:22:29 +0700

    Delete Code backend đăng bài directory

--- Code backend đăng bài/posts_connect.php
+++
@@ -1,4 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "", "data");
-mysqli_set_charset($conn, 'UTF8');
-?>
--- Code backend đăng bài/posts_edit.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Kết nối Database
-require_once 'posts_connect.php';
-$id=$_GET['id'];
-$query=mysqli_query($conn,"select * from `posts` where id='$id'");
-$row=mysqli_fetch_assoc($query);
-?>
-
-<?php require_once 'posts_update.php';?>
